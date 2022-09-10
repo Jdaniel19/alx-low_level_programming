0e26a20b5e99176ac51ddd22aadfe46339f194b6
Jdaniel19 <josephdaniellove@gmail.com>
2022-09-10 03:25:51 -0700

    completed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to standard error.
  * Return: Always 1.
@@ -7,6 +7,6 @@
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	fwrite(2, str1, 59);
+	write(2, str1, 59);
 	return (1);
 }
