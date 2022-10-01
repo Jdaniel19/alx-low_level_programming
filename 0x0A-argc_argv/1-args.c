8a85f952157bdac1566e8d73a77d27a783cb6410
Jdaniel19 <josephdaniellove@gmail.com>
2022-09-30 20:34:59 -0700

    complet

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print number of arg passd
+ * @argc: argument 1
+ * @argv: argument
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
