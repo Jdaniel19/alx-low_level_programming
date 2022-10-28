e67645f2098710ed06124f6e3b15dae611e16dfd
Jdaniel19 <josephdaniellove@gmail.com>
2022-10-27 18:17:15 -0700

    task 0 - binary to unit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_to_uint- convert binary to unsigned int
+ *  @b: binary
+ *  Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int spike = 0, j;
+	unsigned int number = 0, non_binary = 0;
+
+	if (b == NULL)
+		return (non_binary);
+
+	while (b[spike] != '\0')
+		spike++;
+
+	spike -= 1;
+	j = 0;
+
+	while (b[j])
+	{
+		if ((b[j] != '0') && (b[j] != '1'))
+			return (non_binary);
+
+		if (b[j] == '1')
+			number += (1 * (1 << spike));
+
+		j++;
+		spike--;
+	}
+	return (number);
+}
